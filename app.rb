4cb7613571322a419da3b66340c3412b61eff855
Lauren Grant <dangerlauren@gmail.com>
2015-09-18 16:17:52 -0500

    cleaned up code

--- app.rb
+++ app.rb
@@ -9,41 +9,18 @@ end
 
 #routes
 get '/' do
-
 	@growl_directory = session[:growl_directory]
-
 	erb :index
 end
 
-# display the form for adding the growl
-# get '/add_growl' do
-
-# 	erb :add_growl
-# end
-
-# get '/add_growl' do
-
-# 	@growl_directory = session[:growl_directory]
-
-# 	erb :add_growl_success
-
-# end
-
-
 get '/store_growl' do 
 	@growl_directory = session[:growl_directory]
-
 	erb :store_growl
 end
 
 # add a new growl to the page
 post '/add_growl' do 
 	session[:growl_directory] << params[:growl_name]
-
-	# @growl_name = params[:growl_name]
-
-	# erb :add_growl_success
-
 	redirect '/'
 end 
 
