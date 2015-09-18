4594f176a40f6b29332ea0aed4a95063f8d2707a
Lauren Grant <dangerlauren@gmail.com>
2015-09-18 13:38:55 -0500

    stuff is working! doesn't look good yet.

--- app.rb
+++ app.rb
@@ -1,19 +1,34 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+enable :sessions
+
 before do
 	session[:growl_directory] ||= []
 end
 
 #routes
 get '/' do
+
+	@growl_directory = session[:growl_directory]
+
 	erb :index
 end
 
 # display the form for adding the growl
-get '/add_growl' do
-	erb :add_growl
-end
+# get '/add_growl' do
+
+# 	erb :add_growl
+# end
+
+# get '/add_growl' do
+
+# 	@growl_directory = session[:growl_directory]
+
+# 	erb :add_growl_success
+
+# end
+
 
 get '/store_growl' do 
 	@growl_directory = session[:growl_directory]
@@ -25,8 +40,10 @@ end
 post '/add_growl' do 
 	session[:growl_directory] << params[:growl_name]
 
-	@growl_name = params[:growl_name]
+	# @growl_name = params[:growl_name]
 
-	erb :add_growl_success
+	# erb :add_growl_success
+
+	redirect '/'
 end 
 
