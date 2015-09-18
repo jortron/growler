9f983f025312d4feeadc0358f48095a10729af90
Lauren Grant <dangerlauren@gmail.com>
2015-09-18 10:56:59 -0500

    erb files

--- app.rb
+++ app.rb
@@ -2,11 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 before do
-	
+	session[:growl_directory] ||= []
 end
 
 #routes
 get '/' do
-
 	erb :index
 end
+
+# display the form for adding the growl
+get '/add_growl' do
+	erb :add_growl
+end
+
+# add a new growl to the page
+post '/add_growl' do 
+	session[:growl_directory] << params[:growl_name]
+
+	@growl_name = params[:growl_name]
+
+	erb :add_growl_success
+end 
+
